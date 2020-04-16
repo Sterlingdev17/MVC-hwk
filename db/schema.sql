80fff75a56ccc1bf5e2e69d65516ffecde9f915c
Sterling Etienne <sterlingetienne@Sterlings-MacBook-Air.local>
2020-04-15 20:34:27 -0400

    setting up

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE Sterling_burgerDB;
+USE cat_db;
+
+CREATE TABLE burger
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	sleepy BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
